8de4904dbc681fafe3954ff60b6b1b248ae88ef9
Emma Laurijssens van Engelenhoven <emma@talwyn.nl>
2018-11-17 15:09:53 +0100

    ultradistance

--- scripts/rijden.py
+++ scripts/rijden.py
@@ -1,3 +1,10 @@
 from gpiozero import Robot
-robby = Robot(left=(7,8), right=(9,10))
+robby = Robot(left = (7, 8), right = (9, 10))
+while True:
+	robby.forward()
+	sleep(3)
+	robby.stop()
+	robby.right()
+	sleep(1)
+	robby.stop()
 
