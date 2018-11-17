8edb5eb49349bc97862980b6a5c28653a5e0748c
Emma Laurijssens van Engelenhoven <emma@talwyn.nl>
2018-11-17 15:58:21 +0100

    ultradistance

--- scripts/rijden.py
+++ scripts/rijden.py
@@ -1,12 +1,58 @@
 from gpiozero import Robot
-from time import sleep
+import time
+import RPi.GPIO as GPIO
+
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# set GPIO Pins
+GPIO_TRIGGER = 17
+GPIO_ECHO = 27
+
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+def afstand():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
 
 robby = Robot(left = (7, 8), right = (9, 10))
+
+
 while True:
-	robby.forward()
-	sleep(3)
-	robby.stop()
-	robby.right()
-	sleep(1)
-	robby.stop()
+
+    d = afstand()
+
+    if d > 20:
+        snelheid = 1
+    elif d < 10:
+        snelheid = -0.2
+    else:
+        snelheid = 0
+
+    robby.forward(snelheid)
 
