09e2f29850f14a7853cfd10cdc23e30b02a4c2ef
Emma Laurijssens van Engelenhoven <emma@talwyn.nl>
2018-11-17 15:44:34 +0100

    ultradistance

--- scripts/ultragpiozero.py
+++ scripts/ultragpiozero.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(23, 24)
+sensor = DistanceSensor(17, 27)
 
 while True:
     print('Distance to nearest object is', sensor.distance, 'm')
